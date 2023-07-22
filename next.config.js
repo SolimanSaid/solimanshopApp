2f98e9e8e8fd609a0c2833a97ef16de3e81d5ca2
soliman <soliman.farag86@gmail.com>
2023-07-22 04:14:32 +0300

    hello

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co"],
+  },
+};
+
+module.exports = nextConfig;
